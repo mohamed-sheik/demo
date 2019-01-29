ee59c0307af00e797513a74aaec189100b3853db
mohamed-sheik <sheik2557@gmail.com>
2019-01-29 09:10:30 +0530

    add new commit

--- add.c
+++ add.c
@@ -2,13 +2,12 @@
  
 int main() {
    int a, b, sum;
- 
+ Printf("HELLO SHEIK")
    printf("\nEnter two no: ");
    scanf("%d %d", &a, &b);
  
    sum = a + b;
  
    printf("Sum : %d", sum);
- 
    return(0);
 }
